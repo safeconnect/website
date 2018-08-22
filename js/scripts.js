19bf6093e9fc584211224c94d7b21f4db1a637f4
damoon <damoon.azarpazhooh@live.com>
2018-08-22 12:26:53 +0000

    updated

---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+/* Template: Bono - Coming Soon Page Template
+   Author: InovatikThemes
+   Version: 1.0.0
+   Created: Nov 2017
+   Description: Custom JS file
+*/
+
+
+(function($) {
+    "use strict";
+
+	/* Preloader */
+	$(window).on('load', function() {
+		var preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+			}, 500);
+		}
+		hidePreloader();
+	});
+
+
+	/* Countdown Timer */
+	$('#clock').countdown('2018/12/27 08:50:56') /* change here your "countdown to" date */
+	.on('update.countdown', function(event) {
+		var format = '<span class="counter-number">%D<br><span class="timer-text">Days</span></span><span class="counter-number">%H<br><span class="timer-text">Hours</span></span><span class="counter-number">%M<br><span class="timer-text">Minutes</span></span><span class="counter-number">%S<br><span class="timer-text">Seconds</span></span>';
+		$(this).html(event.strftime(format));
+	})
+	.on('finish.countdown', function(event) {
+	$(this).html('This offer has expired!')
+		.parent().addClass('disabled');
+	});
+
+
+	/* Morphtext - Rotating Text In Header */
+	$("#js-rotating").Morphext({
+		// The [in] animation type. Refer to Animate.css for a list of available animations.
+		animation: "fadeIn",
+		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+		separator: ",",
+		// The delay between the changing of each phrase in milliseconds.
+		speed: 2000,
+		complete: function () {
+			// Called after the entrance animation is executed.
+		}
+	});
+
+
+	/* Magnific Popup Lightbox Form */
+	$('.popup-with-move-anim').magnificPopup({
+		type: 'inline',
+		fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+	});
+
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function(){
+		$(this).blur();
+	});
+
+})(jQuery);
